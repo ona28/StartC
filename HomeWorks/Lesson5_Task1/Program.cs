36de1f46cd95724ae4a77f8078443e5c6c150c69
ONA <na.orlova@bk.ru>
2020-12-09 11:48:14 +0300

    обновление vstudio

---
+++ HomeWorks/Lesson5_Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PrivateFunctions;
+
+namespace Lesson5_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SignatureONA.Start("1. Создать программу, которая будет проверять корректность ввода логина. Корректным логином " +
+                "\nбудет строка от 2 до 10 символов, содержащая только буквы латинского алфавита или цифры, " +
+                "\nпри этом цифра не может быть первой: " +
+                "\nа) без использования регулярных выражений; " +
+                "\nб) с использованием регулярных выражений.");
+
+
+            SignatureONA.End();
+        }
+    }
+}
