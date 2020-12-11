bc1c11cf694a5c8bd8b3e0e503349240fb23f38a
ONA <na.orlova@bk.ru>
2020-12-11 17:51:08 +0300

    урок 5 задача 2

---
+++ HomeWorks/Lesson5_Task2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PrivateFunctions;
+
+namespace Lesson5_Task2
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SignatureONA.Start("2. Разработать класс Message, содержащий следующие статические методы для обработки текста: " +
+                "\nа) Вывести только те слова сообщения, которые содержат не более n букв." +
+                "\nб) Удалить из сообщения все слова, которые заканчиваются на заданный символ." +
+                "\nв) Найти самое длинное слово сообщения." +
+                "\nг) Сформировать строку с помощью StringBuilder из самых длинных слов сообщения.");
+
+            string origin = "В лесу родилась ёлочка, в лесу она росла. Зимой и летом стройная, зелёная была. " +
+                            "Метель ей пела песенку, спи ёлочка бай-бай. Мороз снежком укутывал смотри не замерзай.";
+            Console.WriteLine($"Строка - оригинал: {origin}");
+
+            myMessage ms = new myMessage();
+            ms.Txt = string.Copy(origin);
+
+            // а) вывести слова не более n знаков
+            Console.Write("\nа) Введите число букв искомых слов в тексте: ");
+            byte n = Convert.ToByte(Console.ReadLine());
+            string cuts = ms.Cut(n);
+            Console.WriteLine($"Слова не более {n} букв: {cuts}");
+
+            // б) Удалим из сообщения все слова, которые заканчиваются на заданный символ
+            Console.Write("\nб) Удалим из сообщения все слова, которые заканчиваются на символ: ");
+            char ch = Convert.ToChar(Console.ReadLine());
+            string dels = ms.Del(ch);
+            Console.WriteLine($"Результат: {dels}");
+
+            // в) г) Найти самое длинное слово сообщения.
+            (byte, string) maxs = ms.Max();            
+
+            Console.WriteLine($"Самое длинные слова из {maxs.Item1} символов: {maxs.Item2}");
+
+            SignatureONA.End();
+        }
+    }
+}
---
+++ HomeWorks/Lesson5_Task2/myMessage.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Lesson5_Task2
+{
+    class myMessage
+    {
+        private string txt;
+
+        public myMessage() 
+        {
+        }
+
+        public string Txt
+        {
+            get => txt;
+            set => txt = value;
+        }
+
+        private string[] StringToArray()
+        {
+            char[] sep = new char[] { ' ', '.', ',' };
+            string[] wolds = txt.Split(sep);
+
+            return wolds;
+        }
+
+        /// <summary>
+        /// Метод, оставляющий в тексте слова не более n символов
+        /// </summary>
+        /// <param name="n">число символов</param>
+        /// <returns></returns>
+        public string Cut(byte n)
+        {
+            string rezult = "";
+            string[] wolds = StringToArray();
+
+            for (uint i = 0; i < wolds.Length; i++)
+            {
+                if (wolds[i].Length > n || wolds[i].Length == 0) continue;
+                rezult += $"{wolds[i]} ";
+            }
+
+            return rezult;
+        }
+
+        /// <summary>
+        /// Удаление из строки слов, заканчивающихся на заданый символ
+        /// </summary>
+        /// <param name="s">заданый символ</param>
+        /// <returns></returns>
+        public string Del(char s)
+        {
+            string rezult = "";
+
+            Regex myReg = new Regex($@"\w+[{s}]\b", RegexOptions.IgnoreCase);
+            MatchCollection mc = myReg.Matches(txt);
+           
+            foreach (Match match in mc) rezult += $"{match.Value} ";
+            return rezult;
+        }
+
+        /// <summary>
+        /// Поиск максимального слова из текста
+        /// </summary>
+        /// <returns></returns>
+        public (byte, string) Max()
+        {
+            (byte, string) rezult = (0, "");
+            StringBuilder sb = new StringBuilder();
+            string[] wolds = StringToArray();
+
+            if (wolds.Length == 0) return rezult;
+
+            for (uint i = 0; i < wolds.Length; i++) rezult.Item1 = Math.Max(Convert.ToByte(wolds[i].Length), rezult.Item1);
+            for (uint i = 0; i < wolds.Length; i++)
+            {
+                if (Convert.ToByte(wolds[i].Length) == rezult.Item1) sb.Append(wolds[i] + @" ");
+            }
+
+            rezult.Item2 = sb.ToString();
+            return rezult;
+        }
+        
+    }
+}
