85f9e43f536e35082428c2d1f9064a09dbb2a8c0
ONA <na.orlova@bk.ru>
2020-12-18 15:34:20 +0300

    урок 7 задача 2

---
+++ HomeWorks/Lesson7_Task2/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace Lesson7_Task2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHead = new System.Windows.Forms.Label();
+            this.tbAswer = new System.Windows.Forms.TextBox();
+            this.btnRepeat = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblCount = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblMsg = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblHead
+            // 
+            this.lblHead.AutoSize = true;
+            this.lblHead.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblHead.Location = new System.Drawing.Point(26, 22);
+            this.lblHead.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblHead.MinimumSize = new System.Drawing.Size(375, 10);
+            this.lblHead.Name = "lblHead";
+            this.lblHead.Size = new System.Drawing.Size(375, 25);
+            this.lblHead.TabIndex = 0;
+            this.lblHead.Text = "Угадайте число от  0 до 100";
+            this.lblHead.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tbAswer
+            // 
+            this.tbAswer.Location = new System.Drawing.Point(82, 172);
+            this.tbAswer.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tbAswer.Name = "tbAswer";
+            this.tbAswer.Size = new System.Drawing.Size(69, 26);
+            this.tbAswer.TabIndex = 1;
+            // 
+            // btnRepeat
+            // 
+            this.btnRepeat.Location = new System.Drawing.Point(281, 165);
+            this.btnRepeat.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnRepeat.Name = "btnRepeat";
+            this.btnRepeat.Size = new System.Drawing.Size(134, 35);
+            this.btnRepeat.TabIndex = 2;
+            this.btnRepeat.Text = "Начать заново";
+            this.btnRepeat.UseVisualStyleBackColor = true;
+            this.btnRepeat.Click += new System.EventHandler(this.btnRepeat_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 175);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ответ:";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblCount});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 219);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(428, 22);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "прарпар";
+            // 
+            // lblCount
+            // 
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(126, 17);
+            this.lblCount.Text = "Количество попыток:";
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.AutoSize = true;
+            this.lblMsg.ForeColor = System.Drawing.Color.Brown;
+            this.lblMsg.Location = new System.Drawing.Point(91, 103);
+            this.lblMsg.MinimumSize = new System.Drawing.Size(250, 0);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(250, 20);
+            this.lblMsg.TabIndex = 5;
+            this.lblMsg.Text = "!!!";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(27, 56);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.MinimumSize = new System.Drawing.Size(375, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(375, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "(максимум 10 попыток)";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(166, 165);
+            this.btnCheck.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(105, 35);
+            this.btnCheck.TabIndex = 7;
+            this.btnCheck.Text = "Проверить";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(428, 241);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblMsg);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnRepeat);
+            this.Controls.Add(this.tbAswer);
+            this.Controls.Add(this.lblHead);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Угадай число";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHead;
+        private System.Windows.Forms.TextBox tbAswer;
+        private System.Windows.Forms.Button btnRepeat;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblCount;
+        private System.Windows.Forms.Label lblMsg;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCheck;
+    }
+}
+
---
+++ HomeWorks/Lesson7_Task2/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lesson7_Task2
+{
+    public partial class Form1 : Form, IForm
+    {
+        public string Message { set => this.lblMsg.Text = value; }
+        public string Counter { set => this.lblCount.Text = value; }
+        public int Unknown 
+        {
+            set => this.tbAswer.Text = value.ToString();
+            get => Int32.Parse(this.tbAswer.Text); 
+        }        
+
+        Game game;
+
+        public Form1()
+        {
+            InitializeComponent();
+            game = new Game(this);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            game.Start();
+        }
+        
+        private void btnRepeat_Click(object sender, EventArgs e)
+        {
+            game.Start();
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            game.Check();
+        }
+    }
+}
